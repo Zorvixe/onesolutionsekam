c67672729484957ce91ff72040afc5e64f830256
Zorvixe <zorvixe@gmail.com>
2025-09-27 11:16:48 +0530

    first commit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,10 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 
 const Contact = () => {
   useEffect(() => {
-      document.title = "Contact Us| One Solutions";
-    }, []);
+    document.title = "Contact Us | One Solutions";
+  }, []);
+
   return (
     <div>
       {/* Page Title */}
@@ -24,11 +25,11 @@ const Contact = () => {
       <section id="contact" className="contact section">
         <div className="container" data-aos="fade-up" data-aos-delay="100">
           <div className="contact-main-wrapper">
-            
+
             {/* Map */}
             <div className="map-wrapper">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30766863.45380839!2d60.963977322087494!3d19.72451447390969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x417ccb1ab4a05d77%3A0x3444361db5588762!2sOne%20Solutions%20Ekam!5e0!3m2!1sen!2sin!4v1758951890819!5m2!1sen!2sin"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
@@ -42,12 +43,12 @@ const Contact = () => {
             {/* Contact Info */}
             <div className="contact-content">
               <div className="contact-cards-container" data-aos="fade-up" data-aos-delay="300">
-                
+
                 <div className="contact-card">
                   <div className="icon-box"><i className="bi bi-geo-alt"></i></div>
                   <div className="contact-text">
                     <h4>Location</h4>
-                    <p>8721 Broadway Avenue, New York, NY 10023</p>
+                    <p>Hyderabad, Almasguda Telangana, India</p>
                   </div>
                 </div>
 
@@ -55,7 +56,7 @@ const Contact = () => {
                   <div className="icon-box"><i className="bi bi-envelope"></i></div>
                   <div className="contact-text">
                     <h4>Email</h4>
-                    <p>info@examplecompany.com</p>
+                    <p>onesolutions06092024@gmail.com</p>
                   </div>
                 </div>
 
@@ -63,7 +64,7 @@ const Contact = () => {
                   <div className="icon-box"><i className="bi bi-telephone"></i></div>
                   <div className="contact-text">
                     <h4>Call</h4>
-                    <p>+1 (212) 555-7890</p>
+                    <p>+91 8328590444</p>
                   </div>
                 </div>
 
@@ -71,7 +72,7 @@ const Contact = () => {
                   <div className="icon-box"><i className="bi bi-clock"></i></div>
                   <div className="contact-text">
                     <h4>Open Hours</h4>
-                    <p>Monday-Friday: 9AM - 6PM</p>
+                    <p>Monday - Friday: 9AM - 6PM</p>
                   </div>
                 </div>
               </div>
@@ -80,8 +81,9 @@ const Contact = () => {
               <div className="contact-form-container" data-aos="fade-up" data-aos-delay="400">
                 <h3>Get in Touch</h3>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor 
-                  incididunt ut labore et dolore magna aliqua consectetur adipiscing.
+                  Have questions about our training programs, placements, or digital solutions?
+                  Fill out the form below and our team will get back to you within 24 hours.
+                  We’re here to help you start your journey towards a successful career in IT.
                 </p>
 
                 <form className="php-email-form">
