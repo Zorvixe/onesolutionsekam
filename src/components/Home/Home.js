904ccb458d3344151a241ac271d0267313cb80df
Zorvixe <zorvixe@gmail.com>
2025-09-29 00:22:09 +0530

    first commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,8 @@
-import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 
 const Home = () => {
-  useEffect(() => {
-    document.title = "Home | One Solutions";
-  }, []);
+
   return (
     <div>
 
